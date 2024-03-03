b466359af7fbed53e42173d8024cb0a3e0379563
Matheus Lopes <matlopes1999@gmail.com>
2024-03-03 01:14:34 -0300

    add viacep service test

--- internal/services/viacep_test.go
+++ internal/services/viacep_test.go
@@ -1,7 +1,67 @@
 package services
 
-import "testing"
+import (
+	"testing"
 
-func Test_GetCepData(t *testing.T) {
+	"github.com/stretchr/testify/suite"
+)
 
+type CepTestSuit struct {
+	suite.Suite
+	service *viaCEPService
+}
+
+type cepRepositoryMock struct {
+	payload map[string]string
+}
+
+var mock = &cepRepositoryMock{}
+
+func (repo *cepRepositoryMock) GetAddressByCep(cep string) (map[string]string, error) {
+	return repo.payload, nil
+}
+
+func (suite *CepTestSuit) Test_ShouldReturnCepIsNotValid() {
+	mock.payload = map[string]string{
+		"cep": "71111",
+	}
+	data, err := suite.service.GetData(mock.payload["cep"])
+	suite.Nil(data)
+	suite.Error(err, ErrCepNotValid)
+}
+
+func (suite *CepTestSuit) Test_ShouldReturnCepNotFoundError() {
+	mock.payload = map[string]string{
+		"erro": "true",
+	}
+	data, err := suite.service.GetData(mock.payload["cep"])
+	suite.Nil(data)
+	suite.Error(err, ErrCepNotFound)
+}
+
+func (suite *CepTestSuit) Test_ShouldReturnAddressWhenValidCepIsProvided() {
+	mock.payload = map[string]string{
+		"cep":         "01001-000",
+		"logradouro":  "Praça da Sé",
+		"complemento": "lado ímpar",
+		"bairro":      "Sé",
+		"localidade":  "São Paulo",
+		"uf":          "SP",
+		"ibge":        "3550308",
+		"gia":         "1004",
+		"ddd":         "11",
+		"siafi":       "7107",
+	}
+	data, err := suite.service.GetData(mock.payload["cep"])
+	suite.Nil(err)
+	suite.NotNil(data)
+	suite.Equal(data.Cep, "01001000")
+}
+
+func (suite *CepTestSuit) SetupTest() {
+	suite.service = NewViaCepService(mock)
+}
+
+func TestStartSuit(t *testing.T) {
+	suite.Run(t, new(CepTestSuit))
 }
