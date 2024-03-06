de6a27048245473af974ba4140e0fee2b7b09fe9
Matheus Lopes <matlopes1999@gmail.com>
2024-03-05 22:05:46 -0300

    #1: update the code to remove the weather api service from the cep handler

--- internal/infra/web/handler.go
+++ internal/infra/web/handler.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/nimbo1999/temperature-challenge/internal/infra/web/dto"
 	"github.com/nimbo1999/temperature-challenge/internal/repository"
 	"github.com/nimbo1999/temperature-challenge/internal/services"
 )
@@ -31,7 +30,9 @@ func handleCep(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(err.Error() + "\n"))
 		return
 	case nil:
-		break
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(address)
+		return
 	default:
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -39,22 +40,22 @@ func handleCep(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	weatherService := services.NewWeatherService(repository.NewWeatherRepository())
-	temperature, err := weatherService.GetData(*address)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error() + "\n"))
-		return
-	}
+	// weatherService := services.NewWeatherService(repository.NewWeatherRepository())
+	// temperature, err := weatherService.GetData(*address)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	w.Write([]byte(err.Error() + "\n"))
+	// 	return
+	// }
 
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(dto.TemperatureResponseDTO{
-		Celsius:    temperature.Celsius,
-		Fahrenheit: temperature.Fahrenheit,
-		Kelvin:     temperature.Kelvin,
-	})
+	// w.Header().Add("Content-Type", "application/json")
+	// w.WriteHeader(http.StatusOK)
+	// json.NewEncoder(w).Encode(dto.TemperatureResponseDTO{
+	// 	Celsius:    temperature.Celsius,
+	// 	Fahrenheit: temperature.Fahrenheit,
+	// 	Kelvin:     temperature.Kelvin,
+	// })
 }
 
 func (web *WebInfra) ListenAndServe() error {
