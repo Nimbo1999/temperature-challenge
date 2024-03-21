c71cfbe408bb2460fa51c69a03df18640df279ea
Matheus Lopes <matlopes1999@gmail.com>
2024-03-21 09:00:02 -0300

    add zipkin and opentelemetry implementation on cep-service

--- internal/infra/web/cep-handler.go
+++ internal/infra/web/cep-handler.go
@@ -11,9 +11,19 @@ import (
 	"github.com/nimbo1999/temperature-challenge/internal/infra/web/dto"
 	"github.com/nimbo1999/temperature-challenge/internal/services"
 	"github.com/spf13/viper"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func CepHandler(w http.ResponseWriter, r *http.Request) {
+	// Configure span for handler
+	tr := otel.GetTracerProvider().Tracer("cep-handler-component")
+	_, span := tr.Start(r.Context(), "cep-span")
+	defer span.End()
+
+	span.AddEvent("test-event", trace.WithAttributes(attribute.String("Test", "Simple test")))
+
 	var payload dto.CepDTO
 	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
